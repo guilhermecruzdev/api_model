e1fee27cd22c081b901af8b28c4574cd620c06a4
Guilherme Cruz <guilhermecruzdev@gmail.com>
2020-02-19 09:48:23 -0300

    Global Validators - Id and ObjectId

--- utils/joi.js
+++ utils/joi.js
@@ -18,6 +18,12 @@ module.exports = Joi.extend(
         type: 'emails',
     },
 
+    // id
+    {
+        base: Joi.number().strict().min(1),
+        type: 'id',
+    },
+
     // limit
     {
         base: Joi.number().strict().min(10).multiple(10).default(10),
@@ -34,6 +40,12 @@ module.exports = Joi.extend(
     {
         base: Joi.number().strict(),
         type: 'number',
+    },
+
+    // object_id
+    {
+        base: Joi.string().regex(/^[0-9a-fA-F]{24}$/).trim(),
+        type: 'object_id',
     },
 
     // offset
--- validators/GlobalValidator.js
+++ validators/GlobalValidator.js
@@ -2,15 +2,19 @@ const Joi = require('../utils/joi')
 
 module.exports = GlobalValidator = {
 
-    SearchPaginationOrder: Joi.object({
-        search: Joi.search(),
-        offset: Joi.offset(),
-        limit: Joi.limit(),
+    Id: Joi.object({
+        id: Joi.id(),
+    }),
+
+    ObjectId: Joi.object({
+        object_id: Joi.object_id(),
+    }),
+
+    Order: Joi.object({
         order: Joi.order(),
     }),
 
-    SearchPagination: Joi.object({
-        search: Joi.search(),
+    Pagination: Joi.object({
         offset: Joi.offset(),
         limit: Joi.limit(),
     }),
@@ -19,12 +23,16 @@ module.exports = GlobalValidator = {
         search: Joi.search(),
     }),
 
-    Pagination: Joi.object({
+    SearchPagination: Joi.object({
+        search: Joi.search(),
         offset: Joi.offset(),
         limit: Joi.limit(),
     }),
 
-    Order: Joi.object({
+    SearchPaginationOrder: Joi.object({
+        search: Joi.search(),
+        offset: Joi.offset(),
+        limit: Joi.limit(),
         order: Joi.order(),
     }),
 
